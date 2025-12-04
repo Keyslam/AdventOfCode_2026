2c4db4167d28d25da09e8be549a2af0bea3c419d
Keyslam <justinvanderleij@gmail.com>
2025-12-04 11:47:30 +0100

    feat: solve day 4

--- src/day_04/day_04.ts
+++ src/day_04/day_04.ts
@@ -1,15 +1,107 @@
 import { Day, Result } from "../lib/day";
 
+type Tile = {
+    x: number;
+    y: number;
+    hasRoll: boolean;
+};
+type Grid = Tile[][];
+
+const directions = [
+    { dx: 0, dy: -1 },
+    { dx: 0, dy: 1 },
+    { dx: -1, dy: 0 },
+    { dx: 1, dy: 0 },
+
+    { dx: -1, dy: -1 },
+    { dx: 1, dy: -1 },
+    { dx: -1, dy: 1 },
+    { dx: 1, dy: 1 },
+];
+
+function hasLessThan4AdjacentRolls(grid: Grid, tile: Tile): boolean {
+    let adjacentRolls = 0;
+
+    for (const dir of directions) {
+        const x = tile.x + dir.dx;
+        const y = tile.y + dir.dy;
+
+        if (y >= 0 && y < grid.length && x >= 0 && x < grid[y].length) {
+            if (grid[y][x].hasRoll) {
+                adjacentRolls++;
+            }
+        }
+    }
+
+    return adjacentRolls < 4;
+}
+
 export class Day_04 extends Day {
     public part1(input: string): Result {
-        return 0;
+        const grid = this.parseInput(input);
+
+        return grid
+            .flatMap((row) => row)
+            .filter((tile) => {
+                return tile.hasRoll && hasLessThan4AdjacentRolls(grid, tile);
+            })
+            .reduce((acc) => acc + 1, 0);
     }
 
     public part2(input: string): Result {
-        return 0;
+        const grid = this.parseInput(input);
+
+        let count = 0;
+
+        while (true) {
+            const accessibleRolls = grid
+                .flatMap((row) => row)
+                .filter((tile) => {
+                    return (
+                        tile.hasRoll && hasLessThan4AdjacentRolls(grid, tile)
+                    );
+                });
+
+            const iterationCount = accessibleRolls.reduce((acc) => acc + 1, 0);
+
+            accessibleRolls.forEach((tile) => {
+                tile.hasRoll = false;
+            });
+
+            count += iterationCount;
+
+            if (accessibleRolls.length === 0) {
+                break;
+            }
+        }
+
+        return count;
     }
 
     public getDir(): string {
         return __dirname;
+    }
+
+    private parseInput(input: string): Grid {
+        const lines = input.split("\n");
+        const grid: Grid = [];
+
+        for (let y = 0; y < lines.length; y++) {
+            const line = lines[y];
+            const row: Tile[] = [];
+
+            for (let x = 0; x < line.length; x++) {
+                const char = line[x];
+
+                row.push({
+                    x,
+                    y,
+                    hasRoll: char === "@",
+                });
+            }
+            grid.push(row);
+        }
+
+        return grid;
     }
 }
